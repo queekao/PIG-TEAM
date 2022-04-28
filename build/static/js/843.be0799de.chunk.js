d23e2f50adb7db451f74051a769d3763330db61b
queekao <87218631+queekao@users.noreply.github.com>
2022-04-28 11:23:59 +0800

    Delete build directory

--- build/static/js/843.be0799de.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[843],{9843:function(n,a,e){e.r(a),e.d(a,{default:function(){return c}});var o="NotFoundPage_notFound__Aln1z",s=e(1009),t=e(274),u=e(184),c=function(){var n=(0,s.Z)("NotFoundPage"),a=n.data;return n.isLoading?(0,u.jsx)("div",{className:o,children:(0,u.jsx)(t.Z,{})}):a.map((function(n){return(0,u.jsxs)("div",{className:o,children:[(0,u.jsx)("img",{src:n.logoBackground,alt:"Logo"}),(0,u.jsx)("p",{children:n.content})]})}))}}}]);
-//# sourceMappingURL=843.be0799de.chunk.js.map
